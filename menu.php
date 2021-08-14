c8bde2ffa6d8af155e52d7849b5009a266e81f23
Vandijstadt Nicolas <81445233+vandijstadt@users.noreply.github.com>
2021-08-14 13:35:13 +0200

    Delete menu.php

--- menu.php
+++
@@ -1,6 +0,0 @@
-<ul>
-    <?php $nomDePage = str_replace(dirname($_SERVER['PHP_SELF']).'/', '', $_SERVER['PHP_SELF']);?>
-
-    <li <?php if($nomDePage=="index.php"){echo "class=\"active\"" ;} ?>><a href="index.php">Présentation</a></li>
-    <li <?php if($nomDePage=="stream.php"){echo "class=\"active\"" ;} ?>><a href="stream.php">Stream NeyCorp</a></li>
-</ul>
